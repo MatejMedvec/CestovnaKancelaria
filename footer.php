9184ae5695ed5ebd8357cf44e16a9d8e4f52af77
MatejMedvec <161598141+MatejMedvec@users.noreply.github.com>
2024-04-09 22:31:52 +0200

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,1 +1,24 @@
-
+<div class="container-fluid bg-light text-black mt-0">
+      <div class="row p-3">
+        <div class="col-md-4">
+          <h5>Informácie o stránke</h5>
+          <p>Tu môžete vložiť informácie o vašej stránke.</p>
+        </div>
+        <div class="col-md-4">
+          <h5>Ďalšie informácie</h5>
+          <p>Ďalšie dôležité informácie môžete umiestniť sem.</p>
+        </div>
+        <div class="col-md-4">
+          <h5>Kontakt</h5>
+          <p>Pre viac informácii nás môžete kontaktovať <a class="text-reset fw-bold" href="kontakt.html">tu</a></p>
+          <ul class="lisy-group list-group-flush">
+            <p><i class="fa-solid fa-phone me-3"></i><a href="tel:+421904116288">+421 904 116 288</a></p>
+            <p><i class="fa-solid fa-envelope me-3"></i><a href="mailto:matejmedvec@gmail.com">matejmedvec@gmail.com</a>
+            </p>
+          </ul>
+        </div>
+      </div>
+</div>
+<footer class="bg-light text-center p-4">
+    <p class="mb-0 text-body-secondary">© Objavuj Svet. All rights reserved.</p>
+</footer>
