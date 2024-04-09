5479a2f91de25ac7ed815558a7e1645d2728a3f5
MatejMedvec <161598141+MatejMedvec@users.noreply.github.com>
2024-04-09 22:32:34 +0200

    Update galeria.php

--- galeria.php
+++ galeria.php
@@ -13,40 +13,8 @@
   <link href="https://fonts.googleapis.com/css2?family=YourFont&display=swap" rel="stylesheet">
   <title>Objavuj Svet | Galéria</title>
 </head>
-
 <body>
-  <?php include 'connection.php'; ?>
-  <nav class="navbar">
-    <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="index.html">
-          <img src="img/Logo.png" alt="logo" width="50" height="50" class="d-inline-block align-text-top">
-        </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0 ps-3">
-            <li class="nav-item">
-              <a class="nav-link text fw-bold" aria-current="page" href="index.html">Domov</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link text fw-bold" href="o_nas.html">O nás</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link active text fw-bold" href="galeria.html">Galéria</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link text fw-bold" href="blog.html">Blog</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link text fw-bold" href="kontakt.html">Kontakt</a>
-            </li>
-        </div>
-      </div>
-    </nav>
-  </nav>
+  <?php include("header.php"); ?>
   <div class="body">
     <div class="container text-center py-5 mt mt-sm-5">
       <nav aria-label="breadcrumb">
@@ -159,35 +127,13 @@
     </div>
   </div>
   </div>
-  <div class="container-fluid bg-light text-black mt-0">
-    <div class="row p-3">
-      <div class="col-md-4">
-        <h5>Informácie o stránke</h5>
-        <p>Tu môžete vložiť informácie o vašej stránke.</p>
-      </div>
-      <div class="col-md-4">
-        <h5>Ďalšie informácie</h5>
-        <p>Ďalšie dôležité informácie môžete umiestniť sem.</p>
-      </div>
-      <div class="col-md-4">
-        <h5>Kontakt</h5>
-        <p>Pre viac informácii nás môžete kontaktovať <a class="text-reset fw-bold" href="kontakt.html">tu</a></p>
-        <ul class="lisy-group list-group-flush">
-          <p><i class="fa-solid fa-phone me-3"></i><a href="tel:+421904116288">+421 904 116 288</a></p>
-          <p><i class="fa-solid fa-envelope me-3"></i><a href="mailto:matejmedvec@gmail.com">matejmedvec@gmail.com</a>
-          </p>
-        </ul>
-      </div>
-    </div>
+  <?php include("footer.php"); ?>
   </div>
-  <footer class="bg-light text-center p-4">
-    <p class="mb-0 text-body-secondary">© Objavuj Svet. All rights reserved.</p>
-  </footer>
+  <button onclick="topFunction()" id="myBtn" title="Go to top">Naspäť</button>
   <script src="js/app.js"></script>
   <script src="https://kit.fontawesome.com/e3fb291045.js" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
     crossorigin="anonymous"></script>
 </body>
-
 </html>
