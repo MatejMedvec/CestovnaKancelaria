adc5f145da8ad8e43814725af893509609d81ac7
MatejMedvec <161598141+MatejMedvec@users.noreply.github.com>
2024-04-09 22:35:35 +0200

    Update kontakt.php

--- kontakt.php
+++ kontakt.php
@@ -15,38 +15,7 @@
 </head>
 
 <body>
-  <?php include 'connection.php'; ?>
-  <nav class="navbar">
-    <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="index.html">
-          <img src="img/Logo.png" alt="logo" width="50" height="50" class="d-inline-block align-text-top">
-        </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0 ps-3">
-            <li class="nav-item">
-              <a class="nav-link text fw-bold" aria-current="page" href="index.html">Domov</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link text fw-bold" href="o_nas.html">O nás</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link text fw-bold" href="galeria.html">Galéria</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link text fw-bold" href="blog.html">Blog</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link active text fw-bold" href="kontakt.html">Kontakt</a>
-            </li>
-        </div>
-      </div>
-    </nav>
-  </nav>
+  <?php include("header.php"); ?>
   <div class="body">
     <div class="container text-center py-5 mt mt-sm-5">
       <nav aria-label="breadcrumb">
@@ -62,7 +31,7 @@
         <h2 class="text-reset fw-bold text-uppercase pb-2">KONTAKTNÝ FORMULÁR</h2>
       </div>
       <div class="container w-75 mt-5 mb-5 p-5">
-        <form action="thankyou.html">
+        <form action="thankyou.php">
           <div class="mb-3">
             <label for="name" class="form-label">Meno</label>
             <input type="text" class="form-control" id="name" name="name" required>
@@ -85,39 +54,9 @@
         </form>
       </div>
     </div>
-    <div class="container" id="container7">
-    <ul class="list-group text-center">
-      <li class="list-group-item">An item</li>
-      <li class="list-group-item">A second item</li>
-      <li class="list-group-item">A third item</li>
-      <li class="list-group-item">A fourth item</li>
-      <li class="list-group-item">And a fifth one</li>
-    </ul>
   </div>
-</div>
-  <div class="container-fluid bg-light text-black mt-auto">
-    <div class="row p-3">
-      <div class="col-md-4">
-        <h5>Informácie o stránke</h5>
-        <p>Tu môžete vložiť informácie o vašej stránke.</p>
-      </div>
-      <div class="col-md-4">
-        <h5>Ďalšie informácie</h5>
-        <p>Ďalšie dôležité informácie môžete umiestniť sem.</p>
-      </div>
-      <div class="col-md-4">
-        <h5>Kontakt</h5>
-        <p>Pre viac informácii nás môžete kontaktovať <a class="text-reset fw-bold" href="kontakt.html">tu</a></p>
-        <ul class="lisy-group list-group-flush">
-          <p><i class="fa-solid fa-phone me-3"></i><a href="tel:+421904116288">+421 904 116 288</a></p>
-          <p><i class="fa-solid fa-envelope me-3"></i><a href="mailto:matejmedvec@gmail.com">matejmedvec@gmail.com</a></p>
-        </ul>
-      </div>
-    </div>
-  </div>
-  <footer class="bg-light text-center p-4">
-    <p class="mb-0 text-body-secondary">© Objavuj Svet. All rights reserved.</p>
-  </footer>
+  <?php include("footer.php"); ?>
+  <button onclick="topFunction()" id="myBtn" title="Go to top">Naspäť</button>
   <script src="js/app.js"></script>
   <script src="https://kit.fontawesome.com/e3fb291045.js" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
