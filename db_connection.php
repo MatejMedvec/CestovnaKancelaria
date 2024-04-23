982ebff26378309d400fe1df18be8600cd5ca7c1
MatejMedvec <161598141+MatejMedvec@users.noreply.github.com>
2024-04-24 00:14:20 +0200

    Update db_connection.php

--- db_connection.php
+++ db_connection.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "SJ_Projekt";
+$dbname = "sj_projekt";
 
 
 $conn = new mysqli($servername, $username, $password, $dbname);
